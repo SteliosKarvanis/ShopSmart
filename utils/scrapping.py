eedfc906285c812f890538a1d1a0c38a96daaa7e
stelios <stelioscbranco@hotmail.com>
2023-08-18 15:10:18 -0300

    organizing project structure

--- utils/scrapping.py
+++ utils/scrapping.py
@@ -9,7 +9,7 @@ import requests
 import unidecode
 from bs4 import BeautifulSoup
 
-from dtypes import Market, Product
+from utils.dtypes import Market, Product
 
 BASIC_URL = "https://www.ifood.com.br/delivery"
 ## TODO: implement the exceptions handler
