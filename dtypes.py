37d87a386d02182cac6bbe418156353d9d3bf1d0
stelios <stelioscbranco@hotmail.com>
2023-08-23 23:02:49 -0300

    fixing dtypes folder

---
+++ dtypes.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+@dataclass
+class Market:
+    city: str
+    slug_name: str
+    id: str
+    name: str
+    closed: bool
+
+@dataclass
+class Product:
+    description: str
+    details: str
+    logoUrl: str
+    needChoices: str
+    unitPrice: str
+    unitMinPrice: str
+    productTags: str
+    additionalInfo: str
+    category: str
